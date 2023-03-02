aa3c5efd3f43f12bb28f6a4bd938616376eea1af
huangjie <449188650@qq.com>
2023-03-02 17:04:44 +0800

    feat: add build

---
+++ build/precache-manifest.478780b1c31338fad112dde9831fdd0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bf8f8699fa38333488e",
+    "url": "/static/css/main.a4ed5cf7.chunk.css"
+  },
+  {
+    "revision": "1bf8f8699fa38333488e",
+    "url": "/static/js/main.2656e04d.chunk.js"
+  },
+  {
+    "revision": "59b2c92d4d8c1669a47c",
+    "url": "/static/js/runtime~main.1bf0472e.js"
+  },
+  {
+    "revision": "14c82801148cf22593a7",
+    "url": "/static/js/2.8d5ee0e7.chunk.js"
+  },
+  {
+    "revision": "93307e8321b583b7e310",
+    "url": "/static/js/3.c46355a7.chunk.js"
+  },
+  {
+    "revision": "93d0acb0285fee08f7ffd0946af19503",
+    "url": "/index.html"
+  }
+];
